733372040f6d2d574d3edf0bbb26b50ddb09b653
Jon Rafkind <jon@rafkind.com>
2012-11-13 14:19:44 -0700

    make a tree

--- read.rkt
+++ read.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 
+(require racket/list)
 (require parser-tools/lex
          (prefix-in : parser-tools/lex-sre))
 (require racket/match
@@ -9,7 +10,11 @@
 (define-tokens python-tokens (number identifier string))
 
 (define-empty-tokens python-empty-tokens
-  [eof])
+  [eof space tab newline
+   left-bracket right-bracket
+   left-paren right-paren
+   left-brace right-brace
+   colon])
 
 (define-syntax (define-token? stx)
   (syntax-parse stx
@@ -32,9 +37,11 @@
 (define-tokens? eof number identifier 
                 ;end-of-line-comment number string
                 ;block-comment parse-error
-                left-parens right-parens
-                ;left-bracket right-bracket
-                ;left-brace right-brace
+                left-paren right-paren
+                space tab newline
+                left-bracket right-bracket
+                left-brace right-brace
+                colon
                 ;semicolon
                 )
 
@@ -47,12 +54,28 @@
                                              digit))
 (define-lex-abbrev identifier (:: identifier-first-character
                                   (:* identifier-character)))
+(define-lex-abbrev space " ")
+(define-lex-abbrev tab "\t")
+(define-lex-abbrev newline "\n")
+
+(define-lex-abbrev operator (:or "="))
 
 (define python-lexer
   (lexer-src-pos
     [(eof) (token-eof)]
     [number (token-number (string->number lexeme))]
     [identifier (token-identifier (string->symbol lexeme))]
+    [space (token-space)]
+    [tab (token-tab)]
+    ["[" (token-left-bracket)]
+    ["]" (token-right-bracket)]
+    ["(" (token-left-paren)]
+    [")" (token-right-paren)]
+    ["{" (token-left-brace)]
+    ["}" (token-right-brace)]
+    [":" (token-colon)]
+    [newline (token-newline)]
+    [operator (token-identifier (string->symbol lexeme))]
     ))
 
 (define (python-read [port (current-input-port)])
@@ -69,16 +92,62 @@
   (or (token-number? token)
       (token-identifier? token)))
 
-(define (parse tokens)
-  (for/list ([next tokens])
-    (match next
-      [(struct* position-token ([token (and token (? plain-token?))]
-                                [start-pos start]
-                                [end-pos end]))
-       (token-value token)]))) 
+;; returns a tree and an unparsed tree
+(define (parse tokens [delimiter #f])
+  (let loop ([tree '()]
+             [tokens tokens])
+    (if (null? tokens)
+      (values tree '())
+      (let ()
+        (define current (first tokens))
+        ; (printf "Token ~a\n" (token-value (position-token-token current)))
+        (match current
+          [(struct* position-token ([token (and token (? plain-token?))]
+                                    [start-pos start]
+                                    [end-pos end]))
+           (loop (append tree (list (token-value token)))
+                 (cdr tokens))]
+          [(struct* position-token ([token (? token-left-paren?)]
+                                    [start-pos start]
+                                    [end-pos end]))
+           (define-values (sub-tree unparsed) (parse (cdr tokens) 'parens))
+           (loop (append tree (list `(#%parens ,@sub-tree)))
+                 unparsed)]
+
+          [(struct* position-token ([token (? token-right-paren?)]
+                                    [start-pos start]
+                                    [end-pos end]))
+           (when (not (eq? delimiter 'parens))
+             (error 'read "unexpected `)' seen"))
+           (values tree (cdr tokens))]
+
+          [(struct* position-token ([token (? token-left-bracket?)]
+                                    [start-pos start]
+                                    [end-pos end]))
+           (define-values (sub-tree unparsed) (parse (cdr tokens) 'bracket))
+           (loop (append tree (list `(#%brackets ,@sub-tree)))
+                 unparsed)]
+
+          [(struct* position-token ([token (? token-right-bracket?)]
+                                    [start-pos start]
+                                    [end-pos end]))
+           (when (not (eq? delimiter 'bracket))
+             (error 'read "unexpected `]' seen"))
+           (values tree (cdr tokens))]
+
+          [(struct* position-token ([token (and token (or (? token-newline?)
+                                                          (? token-space?)
+                                                          (? token-tab?)))]
+                                    [start-pos start]
+                                    [end-pos end]))
+           (loop tree (cdr tokens))])))))
 
 (define (python-read-string string)
-  (parse (python-read (open-input-string string))))
+  (define-values (tree unparsed)
+                 (parse (python-read (open-input-string string))))
+  (when (not (null? unparsed))
+    (error 'read "unparsed tree" unparsed))
+  tree)
 
 (module+ test
   (require rackunit)
@@ -89,5 +158,25 @@
 
   (check-equal? (python-read-string "x")
                 '(x))
+
+  (check-equal? (python-read-string #<<HERE
+x = 5
+HERE
+)
+                '(x = 5))
+
+(check-equal? (python-read-string #<<HERE
+(x)
+HERE
+)
+
+                '((#%parens x)))
+
+  (check-equal? (python-read-string #<<HERE
+x = [for blah(z) in burger]
+HERE
+)
+
+                '(x = (#%brackets for blah (#%parens z) in burger)))
 
   )
