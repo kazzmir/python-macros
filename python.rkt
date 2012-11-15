1b98dea6c1f97d2fbfe2452a9d1105d2c5aebf44
Jon Rafkind <jon@rafkind.com>
2012-11-15 01:44:22 -0700

    expand def. start enforest

--- python.rkt
+++ python.rkt
@@ -9,6 +9,26 @@
 
 (struct parsed (data))
 
+(define (remove-parsed what)
+  (match what
+    [(struct parsed (data))
+     (remove-parsed data)]
+    [(list x ...)
+     (for/list ([x x])
+       (remove-parsed x))]
+    [x x]))
+
+(define-syntax-rule (debug x ...)
+                    (printf x ...)
+                    #;
+                    (void))
+
+(define (parse-all line environment)
+  (define-values (result rest)
+                 (enforest line environment))
+  (debug "Parse all: result = ~a rest = ~a\n" result rest)
+  result)
+
 (define (enforest input environment)
   (define (parse input precedence left current)
     (match input
@@ -17,8 +37,19 @@
              (list '%block body ...)
              rest ...)
        (define out (parsed `(def ,name ,args (unparsed ,@body))))
-       (values out rest)]))
+       (values out rest)]
+      [(list (and id (? symbol?)) rest ...)
+       (if current
+         (values (left current) input)
+         (parse rest precedence left (parsed id)))]
+      [(list (list line ...) rest ...)
+       (values (parse-all line environment) rest)]
+      [else (error 'enforest "don't know how to enforest ~a" input)]
+      ))
   (parse input 0 (lambda (x) x) #f))
+
+(define (add-lexical! environment name)
+  (hash-set! environment name 'lexical))
 
 ;; python ast
 ;;  (import stuff ...)
@@ -26,15 +57,28 @@
 ;;  (def name (arg ...) stuff ...)
 ;;  (if condition then else)
 ;;  (for z in blah stuf ...)
-;;  (try blah catch (e Except
+;;  (try blah catch (exception) blah)
 ;;  (unparsed stuff ...)
 ;;  (unparsed-class stuff ...)
 (define (expand tree environment)
   (match tree
     [(list 'unparsed stuff ...)
-     (define-values (parsed unparsed)
+     (define-values (result unparsed)
                     (enforest stuff environment))
-     (expand `(,parsed (unparsed ,@unparsed)) environment)]
+     (define out1 (expand (remove-parsed result) environment))
+     (define out2 (if (null? unparsed)
+                    '()
+                    (expand `(unparsed ,@unparsed) environment)))
+     `(,out1 ,@out2)]
+
+    [(and id (? symbol?)) id]
+
+    [(list 'def name (list args ...) body)
+     (debug "Expand def body ~a\n" body)
+     (define body* (expand body environment))
+     (add-lexical! environment name)
+     `(def ,name (,@args) ,body*)]
+
     [else (error 'expand "can't expand ~a" tree)]))
 
 (define (expand-python file)
