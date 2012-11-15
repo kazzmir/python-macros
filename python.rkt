ccd88086f1750cc3f6a224a27718c2b8b88a4ae4
Jon Rafkind <jon@rafkind.com>
2012-11-15 13:04:56 -0700

    enforest operators. expand multiple statements

--- python.rkt
+++ python.rkt
@@ -4,10 +4,32 @@
          racket/match
          racket/cmdline)
 
-(define (base-environment)
-  (make-hash))
-
 (struct parsed (data))
+(struct operator (precedence association binary unary postfix?))
+
+(define (binary-operator precedence association binary [unary #f] [postfix? #f])
+    (operator precedence association binary unary postfix?))
+                  
+(define (unary-operator precedence unary [postfix? #f])
+    (operator precedence 'left #f unary postfix?))
+
+(define (environment-value environment name)
+    (hash-ref environment name (lambda () #f)))
+
+(define (is-operator? what environment)
+    (operator? (environment-value environment what)))
+
+(define python-=
+  (binary-operator 0.1 'left (lambda (left right)
+                               (parsed `(assign ,left ,right)))))
+
+(define (add-operator! environment name value)
+  (hash-set! environment name value))
+
+(define (base-environment)
+  (define environment (make-hash))
+  (add-operator! environment '= python-=)
+  environment)
 
 (define (remove-parsed what)
   (match what
@@ -32,16 +54,67 @@
 (define (enforest input environment)
   (define (parse input precedence left current)
     (match input
+      [(struct parsed (what))
+       (values (left input) #f)]
       [(list 'def (and name (? symbol?))
              (list '#%parens args ...) '%colon
              (list '%block body ...)
              rest ...)
        (define out (parsed `(def ,name ,args (unparsed ,@body))))
        (values out rest)]
+      
+      [(list (and (? (lambda (i)
+                       (is-operator? i environment)))
+                  operator-symbol) rest ...)
+       (define operator (environment-value environment operator-symbol))
+       (define new-precedence (operator-precedence operator))
+       (define association (operator-association operator))
+       (define binary-transformer (operator-binary operator))
+       (define unary-transformer (operator-unary operator))
+       (define postfix? (operator-postfix? operator))
+       (define higher
+         (case association
+           [(left) >]
+           [(right) >=]))
+
+       (if (higher new-precedence precedence)
+         (let-values ([(parsed unparsed)
+                       (parse rest new-precedence
+                              (lambda (stuff)
+                                (define right (parse-all stuff environment))
+                                (define output
+                                  (if current
+                                    (if binary-transformer
+                                      (binary-transformer (parse-all current environment) right)
+                                      (if (and postfix? unary-transformer)
+                                        (unary-transformer (list current))
+                                        (error 'binary "cannot be used as a binary operator in ~a" operator)))
+                                    (if unary-transformer
+                                      (unary-transformer right)
+                                      (error 'unary "cannot be used as a unary operator in ~a" operator))))
+
+                                output)
+                              #f)])
+           (parse unparsed precedence left parsed))
+         (if unary-transformer
+           (if current
+             (values (left current) input)
+             (error 'low-precedence-unary "implement"))
+           (values (left current) input)))]
+
       [(list (and id (? symbol?)) rest ...)
        (if current
          (values (left current) input)
          (parse rest precedence left (parsed id)))]
+
+      [(list (and number (? number?)) rest ...)
+       (if current
+         (values (left current) input)
+         (parse rest precedence left (parsed number)))]
+
+      [(list) (values (left current) input)]
+
+      #;
       [(list (list line ...) rest ...)
        (values (parse-all line environment) rest)]
       [else (error 'enforest "don't know how to enforest ~a" input)]
@@ -61,15 +134,34 @@
 ;;  (unparsed stuff ...)
 ;;  (unparsed-class stuff ...)
 (define (expand tree environment)
+  (debug "Expand ~a\n" tree)
   (match tree
+    [(list 'assign left right)
+     (define left* (expand left environment))
+     (define right* (expand right environment))
+     `(assign ,left* ,right*)]
+
+    [(? number?) tree]
+
     [(list 'unparsed stuff ...)
-     (define-values (result unparsed)
-                    (enforest stuff environment))
-     (define out1 (expand (remove-parsed result) environment))
-     (define out2 (if (null? unparsed)
-                    '()
-                    (expand `(unparsed ,@unparsed) environment)))
-     `(,out1 ,@out2)]
+     (match stuff
+       ;; it might be a list of statements
+       [(list (list first ...) rest ...)
+        (define result (parse-all first environment))
+        (define out1 (expand (remove-parsed result) environment))
+        (define out2 (if (null? rest)
+                       '()
+                       (expand `(unparsed ,@rest) environment)))
+        `(,out1 ,@out2)]
+       [else
+        ;; or just one statement, like a def
+        (define-values (result unparsed)
+                       (enforest stuff environment))
+        (define out1 (expand (remove-parsed result) environment))
+        (define out2 (if (null? unparsed)
+                       '()
+                       (expand `(unparsed ,@unparsed) environment)))
+        `(,out1 ,@out2)])]
 
     [(and id (? symbol?)) id]
 
