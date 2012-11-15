f026885d8f6fcb1ce33b884d99ef0662d04fd4db
Jon Rafkind <jon@rafkind.com>
2012-11-15 13:47:40 -0700

    enforest function calls. add more operators

--- python.rkt
+++ python.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
 (require "read.rkt"
+         racket/pretty
          racket/match
          racket/cmdline)
 
@@ -19,9 +20,17 @@
 (define (is-operator? what environment)
     (operator? (environment-value environment what)))
 
+(define python-and
+  (binary-operator 0.5 'left (lambda (left right)
+                               (parsed `(op and ,left ,right)))))
+
 (define python-=
   (binary-operator 0.1 'left (lambda (left right)
                                (parsed `(assign ,left ,right)))))
+
+(define python-dot
+  (binary-operator 9 'left (lambda (left right)
+                               (parsed `(dot ,left ,right)))))
 
 (define (add-operator! environment name value)
   (hash-set! environment name value))
@@ -29,6 +38,8 @@
 (define (base-environment)
   (define environment (make-hash))
   (add-operator! environment '= python-=)
+  (add-operator! environment 'and python-and)
+  (add-operator! environment '%dot python-dot)
   environment)
 
 (define (remove-parsed what)
@@ -45,11 +56,19 @@
                     #;
                     (void))
 
+(define function-call-precedence 99)
+
 (define (parse-all line environment)
   (define-values (result rest)
                  (enforest line environment))
   (debug "Parse all: result = ~a rest = ~a\n" result rest)
+  (when (and rest (not (null? rest)))
+    (error 'parse-all "could not parse entire expression ~a" rest))
   result)
+
+(define (parse-args args environment)
+  ;; TODO
+  args)
 
 (define (enforest input environment)
   (define (parse input precedence left current)
@@ -61,6 +80,10 @@
              (list '%block body ...)
              rest ...)
        (define out (parsed `(def ,name ,args (unparsed ,@body))))
+       (values out rest)]
+
+      [(list 'import (and name (? symbol?)) rest ...)
+       (define out (parsed `(import ,name)))
        (values out rest)]
       
       [(list (and (? (lambda (i)
@@ -102,6 +125,33 @@
              (error 'low-precedence-unary "implement"))
            (values (left current) input)))]
 
+       [(list (list '#%parens args ...) more ...)
+        (debug "Function call with ~a at ~a\n" current precedence)
+        (if current 
+          (if (> precedence function-call-precedence)
+            (let ()
+              (define function (left current))
+              (define parsed-args (parse-args args environment))
+              (parse more function-call-precedence (lambda (x) x)
+                     (parsed `(call ,function ,@parsed-args))))
+            (let ()
+              (define parsed-args (parse-args args environment))
+              (parse more precedence left (parsed `(call ,current ,@parsed-args)))))
+          ;; not a function call, just parenthesizing an expression
+          (let ()
+            (define inner (parse-all args environment))
+            (parse more precedence left inner)))]
+                                          
+
+      [(list 'return stuff ...)
+       (if current
+         (values (left current) input)
+         (let ()
+           (define-values (returned rest)
+                          (enforest stuff environment))
+           (define out (parsed `(return ,returned)))
+           (values out rest)))]
+
       [(list (and id (? symbol?)) rest ...)
        (if current
          (values (left current) input)
@@ -141,7 +191,25 @@
      (define right* (expand right environment))
      `(assign ,left* ,right*)]
 
+    [(list 'op op left right)
+     (define left* (expand left environment))
+     (define right* (expand right environment))
+     `(op ,op ,left* ,right*)]
+
+    [(list 'dot left right)
+     (define left* (expand left environment))
+     ;; dont expand the right side
+     `(dot ,left* ,right)]
+
     [(? number?) tree]
+
+    [(list 'import what)
+     (add-lexical! environment what)
+     tree]
+
+    [(list 'return what)
+     (define out* (expand what environment))
+     `(return ,out*)]
 
     [(list 'unparsed stuff ...)
      (match stuff
@@ -163,7 +231,11 @@
                        (expand `(unparsed ,@unparsed) environment)))
         `(,out1 ,@out2)])]
 
-    [(and id (? symbol?)) id]
+    [(and id (? symbol?))
+     (when (not (eq? (environment-value environment id)
+                     'lexical))
+       (error 'expand "unbound identifier ~a" id))
+     id]
 
     [(list 'def name (list args ...) body)
      (debug "Expand def body ~a\n" body)
@@ -178,4 +250,4 @@
   (expand `(unparsed ,@tree) (base-environment)))
 
 (define file (command-line #:args (file) file))
-(expand-python file)
+(pretty-print (expand-python file))
